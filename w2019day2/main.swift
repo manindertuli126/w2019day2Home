eb756700ca249b5f93eed4f7880121ac1f2431c0
Maninder Singh <manindersingh@Maninders-Mac.local>
2019-02-09 01:14:49 -0500

    Initial Commit

---
+++ w2019day2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  w2019day2
+//
+//  Created by Maninder Singh on 2019-02-09.
+//  Copyright © 2019 Maninder Singh. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
